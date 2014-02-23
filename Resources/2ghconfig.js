76b7bc645ab9c6e0cd63df9d6fc7c6ddb4ad69b4
SietseB <info@brouwerinbeeld.nl>
2014-02-23 15:26:27 +0100

    init

---
+++ Resources/2ghconfig.js
@@ -0,0 +1,16 @@
+AC.Config.set({
+    apiAdapter : "Web",
+    needFlashVersion : "9.0.0",
+    clientApi : "https://brouwerinbeeld.applicationcraft.com/",
+    serverUrl : "https://brouwerinbeeld.applicationcraft.com/service/",
+    serverUploadUrl : "https://brouwerinbeeld.applicationcraft.com/service/",
+    uploadServerPath: "https://brouwerinbeeld.applicationcraft.com/service/uploads/",
+    uploadServerDirectPath: "https://brouwerinbeeld.applicationcraft.com/service/uploads/direct.php",
+    serverEventsUrl: 'https://notify.applicationcraft.com/events',
+    formatApi : "json",
+    serverThemes: "themes/",
+    localAppsPath: 'Apps/',
+    flashAdapterUrl : "wiziCore/api/server/adapter/WiziSendRequest.swf",
+    isDebugRequest : true,
+    googleApiKey: {"rows":[{"id":1,"data":["http://applicationcraft.com/","ABQIAAAACfMroKb5WzfdVTHbQi2OfRRGQFuqEDC-bwWLPZ60Ces-G56XXhTiOZYFBXlh0_EId2nXJCwq7iTNIg"],"ind":1}]}
+});
